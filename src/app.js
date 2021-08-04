3cf2f3112364ee1e6eeb18d0c442cfdcbec03275
Aurelio Gonzalez <agg156231@gmail.com>
2021-08-03 19:52:53 -0500

    Se implemento cors y se modifico el metodo de entrada de datos por la request
    Se cambio la tipica del formulario a JSON

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express=require('express')
 const app=express()
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-
+const cors=require('cors')
 //importing routes
 const Routes= require('./routes/index.js')
 
 app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
 
 //connecting with mongodb
